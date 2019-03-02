df77d6ecb6ed67c38806556aa8e9935ade302a63
matt1007k <matt1007k@gmail.com>
2019-03-02 02:05:26 -0500

    Validated RegisterInput field

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,29 +1,30 @@
-import {Entity, PrimaryGeneratedColumn, Column, BaseEntity} from "typeorm";
-import { ObjectType, Field } from "type-graphql";
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
+import { ObjectType, Field, Root } from "type-graphql";
 
 @ObjectType()
 @Entity()
-export class User extends BaseEntity{
+export class User extends BaseEntity {
+  @Field()
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Field()
-    @PrimaryGeneratedColumn()
-    id: number;
+  @Field()
+  @Column()
+  firstName: string;
 
-    @Field()
-    @Column()
-    firstName: string;
+  @Field()
+  @Column()
+  lastName: string;
 
-    @Field()
-    @Column()
-    lastName: string;
+  @Field()
+  @Column("text", { unique: true })
+  email: string;
 
-    @Field()
-    @Column('text', { unique: true })
-    email: string;
+  @Field()
+  name(@Root() parent: User): string {
+    return `${parent.firstName} ${parent.lastName}`;
+  }
 
-    @Field()
-    name: string
-
-    @Column()
-    password: string;
+  @Column()
+  password: string;
 }
